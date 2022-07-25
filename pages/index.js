c90fe7f2b5a88e1322960b6f66fc46ba42d1ab82
viet <dangquocviet070394@gmail.com>
2022-07-25 09:28:39 +0700

    fix api

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export default function Home({metaKeyword, datas}) {
         <meta name="description" content="Với định hướng trở thành Công Ty phân phối dược phẩm trong nước số 1 Việt Nam, Công Ty Cổ Phần Dược Pha Nam không ngừng phát triển hệ thống rộng khắp cả nước và luôn hướng tới việc đáp ứng trên mức mong đợi của khách hàng bằng dịch vụ nhanh nhất và độc đáo" />
         <meta name="keywords" content={metaKeyword}/>
         <link rel="icon" href="/favicon.ico" />
+        
       </Head>
       <HomePage datas={datas} />
     </React.Fragment>
@@ -21,7 +22,7 @@ export default function Home({metaKeyword, datas}) {
 export async function getServerSideProps(context) {
   const datas = await axios.get('https://eoffice.merapgroup.com/eoffice/api/api/ecommerce/search-product?limit=1000', {headers: {
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2VvZmZpY2UubWVyYXBncm91cC5jb20vZW9mZmljZS9hcGkvYXBpL2Vjb21tZXJjZS9hdXRoL2xvZ2luIiwiaWF0IjoxNjU1MTkyMjQ4LCJleHAiOjE2NTc4MjAyNDgsIm5iZiI6MTY1NTE5MjI0OCwianRpIjoieHpjT3Z2OGJhWTlNRWZINiIsInN1YiI6IjI3NTQiLCJwcnYiOiIxZDBhMDIwYWNmNWM0YjZjNDk3OTg5ZGYxYWJmMGZiZDRlOGM4ZDYzIn0.1F-6KZKyzg6UPRuOzvixCMSRIb9bpGiTBxHUnhmV3Ys'
+    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2VvZmZpY2UubWVyYXBncm91cC5jb20vZW9mZmljZS9hcGkvYXBpL2Vjb21tZXJjZS9hdXRoL3NpZ24taW4iLCJpYXQiOjE2NTQ2NTU0NzcsImV4cCI6MTk2NTY5NTQ3NywibmJmIjoxNjU0NjU1NDc3LCJqdGkiOiJKRUZCVU9mVjdLTGlrU05kIiwic3ViIjoiMjc2NSIsInBydiI6IjFkMGEwMjBhY2Y1YzRiNmM0OTc5ODlkZjFhYmYwZmJkNGU4YzhkNjMifQ.yUCbgk0eWLEALTz6jmjAnv7uAO_bLd9Qda6O6Hz7JYU'
   }});
 
   const metaKeyword = 'Dược phẩm, phân phối dược, Dược phẩm chức năng, Xisat, Osla';
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -10,10 +10,10 @@ export default function Product({
   return (
     <React.Fragment>
         <Head>
-        <title>{data.product_name}</title>
-        <meta name="description" content={data.uses} />
-        <meta name="keywords" content={data.product_name}/>
-        <link rel="icon" href="/favicon.ico" />
+            <title>{data.product_name}</title>
+            <meta name="description" content={data.uses} />
+            <meta name="keywords" content={data.product_name}/>
+            <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <ProductDetail data={data} />
@@ -26,7 +26,7 @@ export async function getServerSideProps(context) {
     const slug = context.params.slug;
     const datas = await axios.get(`https://eoffice.merapgroup.com/eoffice/api/api/ecommerce/product-detail?slug=${slug}`, {headers: {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2VvZmZpY2UubWVyYXBncm91cC5jb20vZW9mZmljZS9hcGkvYXBpL2Vjb21tZXJjZS9hdXRoL2xvZ2luIiwiaWF0IjoxNjU1MTkyMjQ4LCJleHAiOjE2NTc4MjAyNDgsIm5iZiI6MTY1NTE5MjI0OCwianRpIjoieHpjT3Z2OGJhWTlNRWZINiIsInN1YiI6IjI3NTQiLCJwcnYiOiIxZDBhMDIwYWNmNWM0YjZjNDk3OTg5ZGYxYWJmMGZiZDRlOGM4ZDYzIn0.1F-6KZKyzg6UPRuOzvixCMSRIb9bpGiTBxHUnhmV3Ys'
+      'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2VvZmZpY2UubWVyYXBncm91cC5jb20vZW9mZmljZS9hcGkvYXBpL2Vjb21tZXJjZS9hdXRoL3NpZ24taW4iLCJpYXQiOjE2NTQ2NTU0NzcsImV4cCI6MTk2NTY5NTQ3NywibmJmIjoxNjU0NjU1NDc3LCJqdGkiOiJKRUZCVU9mVjdLTGlrU05kIiwic3ViIjoiMjc2NSIsInBydiI6IjFkMGEwMjBhY2Y1YzRiNmM0OTc5ODlkZjFhYmYwZmJkNGU4YzhkNjMifQ.yUCbgk0eWLEALTz6jmjAnv7uAO_bLd9Qda6O6Hz7JYU'
     }});
     
     return {
